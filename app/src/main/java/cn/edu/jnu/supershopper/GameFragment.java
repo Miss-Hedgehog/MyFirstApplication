dca6d8cb4551a218d7feaca91b62bbd9726cf906
Miss-Hedgehog <271551725@qq.com>
2022-11-20 10:03:45 +0800

    GameFragment

---
+++ app/src/main/java/cn/edu/jnu/supershopper/GameFragment.java
@@ -0,0 +1,52 @@
+package cn.edu.jnu.supershopper;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import cn.edu.jnu.supershopper.views.GameView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GameFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GameFragment extends Fragment {
+    public GameFragment() {
+        // Required empty public constructor
+    }
+    // TODO: Rename and change types and number of parameters
+    public static GameFragment newInstance() {
+        GameFragment fragment = new GameFragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_game, container, false);
+
+        return rootView;
+
+
+    }
+}
